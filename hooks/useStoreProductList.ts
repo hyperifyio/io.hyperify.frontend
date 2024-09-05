38d04b6f05f06f9d6fb898c447db6bd164839fe5
Jaakko Heusala <jhh@hg.fi>
2024-09-05 13:58:09 +0300

    Improved email features table for https://github.com/sendanor/project-sendanor.fi/issues/251

--- hooks/useStoreProductList.ts
+++ hooks/useStoreProductList.ts
@@ -1,11 +1,15 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
 import { useEffect, useState } from "react";
+import { sortBy } from "../../core/functions/sortBy";
 import { StoreProductService } from "../../core/StoreProductService";
 import { Product } from "../../core/store/types/product/Product";
 import { ProductType } from "../../core/store/types/product/ProductType";
 
-export function useStoreProductList (type ?: ProductType) : Product[] {
+export function useStoreProductList (
+    type ?: ProductType,
+    sortByKey ?: string,
+) : Product[] {
     const [ sellerMap, setSendanorProductList ] = useState<Product[]>(type ? StoreProductService.getProductsByType(type) : StoreProductService.getAllProducts() );
     // When session service changes data
     useEffect(() => {
@@ -15,7 +19,13 @@ export function useStoreProductList (type ?: ProductType) : Product[] {
         return StoreProductService.on(
             StoreProductService.Event.UPDATED,
             () => {
-                setSendanorProductList( type ? StoreProductService.getProductsByType(type) : StoreProductService.getAllProducts() );
+                let list = type ? StoreProductService.getProductsByType(type) : StoreProductService.getAllProducts();
+                if (sortByKey) {
+                    list = sortBy(list, [sortByKey])
+                    setSendanorProductList( list );
+                } else {
+                    setSendanorProductList( list );
+                }
             }
         );
     });
